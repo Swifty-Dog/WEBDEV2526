09c1da25f0e13774c331e6f0e85d81e089a968ea
NoLimitJath <jathjackrich2@gmail.com>
2025-11-12 15:38:17 +0100

    added files

---
+++ OfficeCalendar.API/OfficeCalendar.API/Controllers/EventController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+namespace OfficeCalendar.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class EventController : BaseController
+{
+    public EventController() : base() { }
+}
+
+
+
---
+++ OfficeCalendar.API/OfficeCalendar.API/Services/EventService.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.Design;
+using OfficeCalendar.API.Services.Interfaces;
+
+namespace OfficeCalendar.API.Services;
+
+public class EventService : IEventService
+{
+
+}
