03f22e0c9d8c94aa5267c04b12434042e9be3f0b
Jason <jasonvuong@live.nl>
2026-01-02 21:31:58 +0100

    test

--- OfficeCalendar.API/OfficeCalendar.API/DTOs/Employees/Request/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-namespace OfficeCalendar.API.DTOs.Employees.Request;
-
-public class RegisterDto
-{
-    public required string FirstName { get; set; } = string.Empty;
-    public required string LastName { get; set; } = string.Empty;
-    public required string Email { get; set; } = string.Empty;
-    public required string Password { get; set; } = string.Empty;
-}
